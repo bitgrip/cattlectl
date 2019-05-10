3d09d5b3fbfbf729ab73eace3a2bc31c4b7a3ed9
Wulf-Thilo Schreiter <wulf-thilo.schreiter@bitgrip.de>
2019-05-10 12:43:31 +0200

    Add support for multiple values files (#25)
    * The flag  can be used multiple times.
    * The content of the files is merged.
    * The following files may overwrite values from former files

--- cmd/utils/values_test.go
+++ cmd/utils/values_test.go
@@ -26,6 +26,9 @@ import (
 func TestComplexValuesStructure(t *testing.T) {
 	const valuesFile = "testdata/values-with-structure.yaml"
 	os.Setenv("STORAGE_CLASS_AZURE_VOLUME_BINDING_MODE", "changed-bind-mode")
+	defer func() {
+		os.Unsetenv("STORAGE_CLASS_AZURE_VOLUME_BINDING_MODE")
+	}()
 
 	expected := make(map[string]interface{}, 0)
 	fileContent, err := ioutil.ReadFile(valuesFile)
@@ -65,6 +68,9 @@ func TestPlainValuesStructure(t *testing.T) {
 func TestChangeValueByEnvironmentVariable(t *testing.T) {
 	const valuesFile = "testdata/values.yaml"
 	os.Setenv("KEY1", "altered-by-env")
+	defer func() {
+		os.Unsetenv("KEY1")
+	}()
 
 	expected := make(map[string]interface{}, 0)
 	fileContent, err := ioutil.ReadFile(valuesFile)
@@ -85,13 +91,6 @@ func TestFailWhenValuesKeysAreMissformated(t *testing.T) {
 	assert.NotOk(t, err, "uppercase characters are not allowed on value keys")
 }
 
-func TestBla(t *testing.T) {
-	const valuesFile = "testdata/values-with-structure.yaml"
-
-	_, err := LoadValues(valuesFile)
-	assert.Ok(t, err)
-}
-
 func TestNotExistingValuesFile(t *testing.T) {
 	const valuesFile = "testdata/not-existing-values.yaml"
 
@@ -99,5 +98,35 @@ func TestNotExistingValuesFile(t *testing.T) {
 
 	actual, err := LoadValues(valuesFile)
 	assert.Ok(t, err)
+	assert.Equals(t, expected, actual)
+}
+
+func TestEmptyFileList(t *testing.T) {
+	expected := make(map[string]interface{}, 0)
+
+	actual, err := LoadValues()
+	assert.Ok(t, err)
+	assert.Equals(t, expected, actual)
+}
+
+func TestMultipleFileList(t *testing.T) {
+	const valuesFile1 = "testdata/values.1.yaml"
+	const valuesFile2 = "testdata/values.2.yaml"
+	const valuesFileExpected = "testdata/values.expected.yaml"
+
+	expected := make(map[string]interface{}, 0)
+
+	fileContent, err := ioutil.ReadFile(valuesFileExpected)
+	assert.Ok(t, err)
+	err = yaml.Unmarshal(fileContent, &expected)
+	assert.Ok(t, err)
+
+	actual, err := LoadValues(valuesFile1, valuesFile2)
+	assert.Ok(t, err)
+
+	actualData, err := yaml.Marshal(actual)
+	assert.Ok(t, err)
+	err = yaml.Unmarshal(actualData, &actual)
+
 	assert.Equals(t, expected, actual)
 }
