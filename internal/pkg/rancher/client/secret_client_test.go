e114656f8907c50171b155188b250517ab0b9010
Wulf-Thilo Schreiter <wt.schreiter@gmail.com>
2019-05-18 23:51:23 +0200

    Add SecretClient

---
+++ internal/pkg/rancher/client/secret_client_test.go
@@ -0,0 +1,190 @@
+// Copyright © 2019 Bitgrip <berlin@bitgrip.de>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package client
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+
+	"github.com/bitgrip/cattlectl/internal/pkg/assert"
+	projectModel "github.com/bitgrip/cattlectl/internal/pkg/rancher/project/model"
+	"github.com/bitgrip/cattlectl/internal/pkg/rancher/stubs"
+	"github.com/rancher/norman/types"
+	backendClient "github.com/rancher/types/client/project/v3"
+	"github.com/sirupsen/logrus"
+)
+
+func Test_secretClient_Exists(t *testing.T) {
+	tests := []struct {
+		name      string
+		client    *secretClient
+		wanted    bool
+		wantErr   bool
+		wantedErr string
+	}{
+		{
+			name: "Existing",
+			client: existingSecretClient(
+				t,
+				&types.ListOpts{
+					Filters: map[string]interface{}{
+						"name":        "existing-secret",
+						"namespaceId": "test-namespace-id",
+					},
+				},
+			),
+			wanted:  true,
+			wantErr: false,
+		},
+		{
+			name: "Not_Existing",
+			client: notExistingSecretClient(
+				t,
+				&types.ListOpts{
+					Filters: map[string]interface{}{
+						"name":        "existing-secret",
+						"namespaceId": "test-namespace-id",
+					},
+				},
+			),
+			wanted:  false,
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := tt.client.Exists()
+			if tt.wantErr {
+				assert.NotOk(t, err, tt.wantedErr)
+			} else {
+				assert.Ok(t, err)
+				assert.Equals(t, tt.wanted, got)
+			}
+		})
+	}
+}
+
+func Test_secretClient_Create(t *testing.T) {
+	tests := []struct {
+		name      string
+		client    *secretClient
+		wantErr   bool
+		wantedErr string
+	}{
+		{
+			name: "Create",
+			client: notExistingSecretClient(
+				t,
+				nil,
+			),
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.client.Create()
+			if tt.wantErr {
+				assert.NotOk(t, err, tt.wantedErr)
+			} else {
+				assert.Ok(t, err)
+			}
+		})
+	}
+}
+
+func existingSecretClient(t *testing.T, expectedListOpts *types.ListOpts) *secretClient {
+	const (
+		projectID   = "test-project-id"
+		projectName = "test-project-name"
+		namespaceID = "test-namespace-id"
+		namespace   = "test-namespace"
+		clusterID   = "test-cluster-id"
+		secretName  = "test-secret"
+	)
+	var (
+		secret      = projectModel.ConfigMap{}
+		testClients = stubs.CreateBackendStubs(t)
+	)
+	secret.Name = secretName
+
+	secretOperationsStub := stubs.CreateSecretOperationsStub(t)
+	secretOperationsStub.DoList = func(opts *types.ListOpts) (*backendClient.SecretCollection, error) {
+		if !reflect.DeepEqual(expectedListOpts, opts) {
+			return nil, fmt.Errorf("Unexpected ListOpts %v", opts)
+		}
+		return &backendClient.SecretCollection{
+			Data: []backendClient.Secret{
+				backendClient.Secret{
+					Name:        "existing-secret",
+					NamespaceId: "test-namespace-id",
+				},
+			},
+		}, nil
+	}
+	testClients.ProjectClient.Secret = secretOperationsStub
+	result, err := newSecretClient(
+		"existing-secret",
+		"test-namespace",
+		nil,
+		testClients.ProjectClient,
+		logrus.New().WithFields(logrus.Fields{}),
+	)
+	assert.Ok(t, err)
+	secretClientResult := result.(*secretClient)
+	secretClientResult.namespaceID = "test-namespace-id"
+	return secretClientResult
+}
+
+func notExistingSecretClient(t *testing.T, expectedListOpts *types.ListOpts) *secretClient {
+	const (
+		projectID   = "test-project-id"
+		projectName = "test-project-name"
+		namespaceID = "test-namespace-id"
+		namespace   = "test-namespace"
+		clusterID   = "test-cluster-id"
+		secretName  = "test-secret"
+	)
+	var (
+		secret      = projectModel.ConfigMap{}
+		testClients = stubs.CreateBackendStubs(t)
+	)
+	secret.Name = secretName
+
+	secretOperationsStub := stubs.CreateSecretOperationsStub(t)
+	secretOperationsStub.DoList = func(opts *types.ListOpts) (*backendClient.SecretCollection, error) {
+		if !reflect.DeepEqual(expectedListOpts, opts) {
+			return nil, fmt.Errorf("Unexpected ListOpts %v", opts)
+		}
+		return &backendClient.SecretCollection{
+			Data: []backendClient.Secret{},
+		}, nil
+	}
+	secretOperationsStub.DoCreate = func(secret *backendClient.Secret) (*backendClient.Secret, error) {
+		return secret, nil
+	}
+	testClients.ProjectClient.Secret = secretOperationsStub
+	result, err := newSecretClient(
+		"existing-secret",
+		"test-namespace",
+		&projectClient{},
+		testClients.ProjectClient,
+		logrus.New().WithFields(logrus.Fields{}),
+	)
+	assert.Ok(t, err)
+	secretClientResult := result.(*secretClient)
+	secretClientResult.namespaceID = "test-namespace-id"
+	return secretClientResult
+}
