180f8edfb704afaf47c01603f7c7c7daa56676fe
Wulf-Thilo Schreiter <wt.schreiter@gmail.com>
2019-05-19 14:40:15 +0200

    Add storage class and persistent volume clients

---
+++ internal/pkg/rancher/client/persistent_volume_client.go
@@ -0,0 +1,141 @@
+// Copyright © 2019 Bitgrip <berlin@bitgrip.de>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package client
+
+import (
+	"fmt"
+
+	projectModel "github.com/bitgrip/cattlectl/internal/pkg/rancher/project/model"
+	"github.com/rancher/norman/types"
+	backendClient "github.com/rancher/types/client/cluster/v3"
+	"github.com/sirupsen/logrus"
+)
+
+func newPersistentVolumeClientWithData(
+	persistentVolume projectModel.PersistentVolume,
+	namespace string,
+	project ProjectClient,
+	backendClient *backendClient.Client,
+	logger *logrus.Entry,
+) (PersistentVolumeClient, error) {
+	result, err := newPersistentVolumeClient(
+		persistentVolume.Name,
+		namespace,
+		project,
+		backendClient,
+		logger,
+	)
+	if err != nil {
+		return nil, err
+	}
+	err = result.SetData(persistentVolume)
+	return result, err
+}
+
+func newPersistentVolumeClient(
+	name, namespace string,
+	project ProjectClient,
+	backendClient *backendClient.Client,
+	logger *logrus.Entry,
+) (PersistentVolumeClient, error) {
+	return &persistentVolumeClient{
+		resourceClient: resourceClient{
+			name:   name,
+			logger: logger.WithField("persistentVolume_name", name).WithField("namespace", namespace),
+		},
+		backendClient: backendClient,
+	}, nil
+}
+
+type persistentVolumeClient struct {
+	resourceClient
+	persistentVolume projectModel.PersistentVolume
+	backendClient    *backendClient.Client
+}
+
+func (client *persistentVolumeClient) init() error {
+	return nil
+}
+
+func (client *persistentVolumeClient) Exists() (bool, error) {
+	if err := client.init(); err != nil {
+		return false, err
+	}
+	collection, err := client.backendClient.PersistentVolume.List(&types.ListOpts{
+		Filters: map[string]interface{}{
+			"name": client.name,
+		},
+	})
+	if nil != err {
+		client.logger.WithError(err).Error("Failed to read persistentVolume list")
+		return false, fmt.Errorf("Failed to read persistentVolume list, %v", err)
+	}
+	for _, item := range collection.Data {
+		if item.Name == client.name {
+			return true, nil
+		}
+	}
+	client.logger.Debug("PersistentVolume not found")
+	return false, nil
+}
+
+func (client *persistentVolumeClient) Create() error {
+	if err := client.init(); err != nil {
+		return err
+	}
+	client.logger.Info("Create new persistent volume")
+	newPersistentVolume := &backendClient.PersistentVolume{
+		Name:                          client.persistentVolume.Name,
+		StorageClassID:                client.persistentVolume.StorageClassName,
+		AccessModes:                   client.persistentVolume.AccessModes,
+		Capacity:                      map[string]string{"storage": client.persistentVolume.Capacity},
+		PersistentVolumeReclaimPolicy: "Delete",
+		Local: &backendClient.LocalVolumeSource{
+			Path: client.persistentVolume.Path,
+		},
+		NodeAffinity: &backendClient.VolumeNodeAffinity{
+			Required: &backendClient.NodeSelector{
+				NodeSelectorTerms: []backendClient.NodeSelectorTerm{
+					backendClient.NodeSelectorTerm{
+						MatchExpressions: []backendClient.NodeSelectorRequirement{
+							backendClient.NodeSelectorRequirement{
+								Key:      "kubernetes.io/hostname",
+								Operator: "In",
+								Values:   []string{client.persistentVolume.Node},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	_, err := client.backendClient.PersistentVolume.Create(newPersistentVolume)
+	return err
+}
+
+func (client *persistentVolumeClient) Upgrade() error {
+	return fmt.Errorf("upgrade statefulset not supported")
+}
+
+func (client *persistentVolumeClient) Data() (projectModel.PersistentVolume, error) {
+	return client.persistentVolume, nil
+}
+
+func (client *persistentVolumeClient) SetData(persistentVolume projectModel.PersistentVolume) error {
+	client.name = persistentVolume.Name
+	client.persistentVolume = persistentVolume
+	return nil
+}
---
+++ internal/pkg/rancher/client/storage_class_client.go
@@ -0,0 +1,124 @@
+// Copyright © 2019 Bitgrip <berlin@bitgrip.de>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package client
+
+import (
+	"fmt"
+
+	projectModel "github.com/bitgrip/cattlectl/internal/pkg/rancher/project/model"
+	"github.com/rancher/norman/types"
+	backendClient "github.com/rancher/types/client/cluster/v3"
+	"github.com/sirupsen/logrus"
+)
+
+func newStorageClassClientWithData(
+	storageClass projectModel.StorageClass,
+	namespace string,
+	project ProjectClient,
+	backendClient *backendClient.Client,
+	logger *logrus.Entry,
+) (StorageClassClient, error) {
+	result, err := newStorageClassClient(
+		storageClass.Name,
+		namespace,
+		project,
+		backendClient,
+		logger,
+	)
+	if err != nil {
+		return nil, err
+	}
+	err = result.SetData(storageClass)
+	return result, err
+}
+
+func newStorageClassClient(
+	name, namespace string,
+	project ProjectClient,
+	backendClient *backendClient.Client,
+	logger *logrus.Entry,
+) (StorageClassClient, error) {
+	return &storageClassClient{
+		resourceClient: resourceClient{
+			name:   name,
+			logger: logger.WithField("storageClass_name", name).WithField("namespace", namespace),
+		},
+		backendClient: backendClient,
+	}, nil
+}
+
+type storageClassClient struct {
+	resourceClient
+	storageClass  projectModel.StorageClass
+	backendClient *backendClient.Client
+}
+
+func (client *storageClassClient) init() error {
+	return nil
+}
+
+func (client *storageClassClient) Exists() (bool, error) {
+	if err := client.init(); err != nil {
+		return false, err
+	}
+	collection, err := client.backendClient.StorageClass.List(&types.ListOpts{
+		Filters: map[string]interface{}{
+			"name": client.name,
+		},
+	})
+	if nil != err {
+		client.logger.WithError(err).Error("Failed to read storageClass list")
+		return false, fmt.Errorf("Failed to read storageClass list, %v", err)
+	}
+	for _, item := range collection.Data {
+		if item.Name == client.name {
+			return true, nil
+		}
+	}
+	client.logger.Debug("StorageClass not found")
+	return false, nil
+}
+
+func (client *storageClassClient) Create() error {
+	if err := client.init(); err != nil {
+		return err
+	}
+	client.logger.Info("Create new storage class")
+	newStorageClass := &backendClient.StorageClass{
+		Name:              client.storageClass.Name,
+		VolumeBindingMode: client.storageClass.VolumeBindMode,
+		ReclaimPolicy:     client.storageClass.ReclaimPolicy,
+		Provisioner:       client.storageClass.Provisioner,
+		Parameters:        client.storageClass.Parameters,
+		MountOptions:      client.storageClass.MountOptions,
+	}
+
+	_, err := client.backendClient.StorageClass.Create(newStorageClass)
+	return err
+}
+
+func (client *storageClassClient) Upgrade() error {
+	return fmt.Errorf("upgrade statefulset not supported")
+}
+
+func (client *storageClassClient) Data() (projectModel.StorageClass, error) {
+	return client.storageClass, nil
+}
+
+func (client *storageClassClient) SetData(storageClass projectModel.StorageClass) error {
+	client.name = storageClass.Name
+	client.storageClass = storageClass
+	return nil
+}
